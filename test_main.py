b91929aa14bf0c306d49d97c16605f401f89f9f6
Luis A. Duque C <luaduqueca@gmail.com>
2022-02-20 01:26:34 -0500

    fix issue in _testing_pairs_of_players function

--- test_main.py
+++ test_main.py
@@ -2,8 +2,11 @@ import pytest
 from main import pairs_of_players
 
 def _testing_pairs_of_players(my_dict,expected):
-    comparision = [(i in expected.items() or i in dict((y,x) for x,y in expected.items()).items()) for i in my_dict.items() if len(my_dict)==len(expected)]
-    return (all(comparision))
+    comparision = [(i in expected.items() or i in dict((y,x) for x,y in expected.items()).items()) for i in my_dict.items()]
+    if all(comparision) and (len(comparision) == len(my_dict)) and (len(comparision) == len(expected)):
+        return(True)
+    else:
+        return(False)
 
 def test_pairs_of_players():
     assert _testing_pairs_of_players(my_dict = pairs_of_players(139), expected = {"Brevin Knight":"Nate Robinson", "Nate Robinson":"Mike Wilks"}) == True
