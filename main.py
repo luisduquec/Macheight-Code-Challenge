e75e6ee63a929a637a921717daa59ab337bd2209
Luis A. Duque C <luaduqueca@gmail.com>
2022-02-20 00:39:21 -0500

    v2 : add unit test and format function

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import json
 import requests
-
 response = requests.get("https://mach-eight.uc.r.appspot.com/")
 file = response.json()
 values = list(file.values())
-values = values[0]
+values_sorted = sorted(values[0], key = lambda p: p["h_in"])
 
 def pairs_of_players(h_in):
-    pairs = [  ( " ".join((i["first_name"], i["last_name"])), " ".join((j["first_name"], j["last_name"])) ) for i in values for j in values if int(i["h_in"]) + int(j["h_in"]) == h_in ]
-    if len(pairs)>1:
-        for i in pairs:
-            for j in pairs:
-                if ((i[0] == j[1] and i[1]==j[0] and pairs.index(j) > pairs.index(i)) or (j[0]==j[1])):
-                    pairs.remove(j)
-        answer =""
-        for p in pairs:
-            answer = answer + "- " + p[0] + " "*9 + p[1] + "\n"
-        return answer
-    else:
+    if (h_in < int(values_sorted[0]["h_in"]) + int(values_sorted[1]["h_in"]))  or (h_in > int(values_sorted[-2]["h_in"]) + int(values_sorted[-1]["h_in"]))  :
         return("No matches found")
+    else:
+        pairs = [  ( " ".join((i["first_name"], i["last_name"])), " ".join((j["first_name"], j["last_name"])) ) for i in values_sorted for j in values_sorted if int(i["h_in"]) + int(j["h_in"]) == h_in ]
+        unique_pairs = dict(set(tuple(sorted(x)) for x in pairs))
+        if len(unique_pairs)==0:
+            return ("No matches found")
+        return (unique_pairs)
+def format_output(dicti):
+    try:
+        for k, v in dicti.items():
+            print("- {:<20} {:<20}".format(k,v))
+    except:
+        print(dicti)
+
 ##commented lines to allow unit tests to work correctly. Please remove the # below in case of executing the code
-#h_in = int(input("Hello an Welcome to the App!\nPlease enter a number to see the pairs of players: "))
-#print(pairs_of_players(h_in))
+#h_in = int(input("Hello and Welcome to the App!\nPlease enter a number to see the pairs of players: "))
+#format_output(pairs_of_players(h_in))
--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
 import pytest
 from main import pairs_of_players
 
+def _testing_pairs_of_players(my_dict,expected):
+    comparision = [(i in expected.items() or i in dict((y,x) for x,y in expected.items()).items()) for i in my_dict.items() if len(my_dict)==len(expected)]
+    return (all(comparision))
+
 def test_pairs_of_players():
-    assert pairs_of_players(139) == "- Brevin Knight    Nate Robinson\n- Nate Robinson    Mike Wilks\n"
+    assert _testing_pairs_of_players(my_dict = pairs_of_players(139), expected = {"Brevin Knight":"Nate Robinson", "Nate Robinson":"Mike Wilks"}) == True
